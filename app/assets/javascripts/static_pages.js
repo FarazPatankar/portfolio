156b2951bb5e702935fa05fad5c44fe84838882d
Faraz Patankar <farazpatankar@gmail.com>
2016-06-08 16:39:35 +0530

    set up project with materialize and typed

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,13 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+    $(function() {
+        $(".typed").typed({
+            strings: ["Ruby on Rails^500", "Node JS^500", "React JS^500", "Aspiring Entrepreneur^500", "Tech Enthusiast^500", "Avid Learner^500"],
+            typeSpeed: 30,
+            backSpeed: 30,
+            loop: true
+        });
+    });
+});
